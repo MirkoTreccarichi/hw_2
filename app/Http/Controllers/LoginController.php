fba4e580bdc55e033524d813f594000dacc514f4
MIRKO TRECCARICHI <uni400949@studium.unict.it>
2021-06-10 17:08:37 +0200

    LoginController.php created

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+class LoginController extends Controller
+{
+    function login(){
+        return redirect();
+    }
+
+    function checkLogin(){
+//        todo funzione checklogin controller
+    }
+    function logout(){
+//        todo funzione logout controller
+    }
+
+}
