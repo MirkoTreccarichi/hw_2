c4054c450e51f4a3599d90e81ff7a9eeec476150
MIRKO TRECCARICHI <uni400949@studium.unict.it>
2021-06-10 18:55:24 +0200

    function page defined

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,5 +6,11 @@ namespace App\Http\Controllers;
 
 class RegisterController extends Controller
 {
+    function register(){
 
+    }
+
+    function page(){
+        return view('signup');
+    }
 }
