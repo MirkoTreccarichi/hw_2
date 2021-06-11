8032e576412e3c90cfa0c14457a1df393d0fad05
MIRKO TRECCARICHI <uni400949@studium.unict.it>
2021-06-11 16:04:13 +0200

    todo added

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -7,7 +7,7 @@ namespace App\Http\Controllers;
 class RegisterController extends Controller
 {
     function register(){
-
+    //todo funzione di registrazione nel db
     }
 
     function page(){
