2e7d6d124d3bb76c01db59b03d0bc001f5c0ab06
MIRKO TRECCARICHI <uni400949@studium.unict.it>
2021-06-15 17:10:28 +0200

    style fixed

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -6,6 +6,9 @@
 @push('scripts')
     <script defer src = '{{asset('js/signup.js')}}'></script>
 @endpush
+@push('styles')
+    <link rel="stylesheet" href="{{asset('styles/signup.css')}}">
+@endpush
 @section('content')
     <!-- SECTION -->
     <section>
@@ -14,48 +17,45 @@
             <img class="logoimg" src="img/Smartket/SmartketWhite.png">
             <div class="comp">Smartket</div>
         </div>
-        <!-- FORM -->
-        <form name='signup' method='post'>
-            <!-- NAME -->
-            <div class="name">
-                <label for="name">Nome</label>
-                <input type="text" name="name">
-            </div>
-            <!-- SURNAME -->
-            <div class="surname">
-                <label for="surname">Cognome</label>
-                <input type="text" name="surname">
-            </div>
-            <!-- BIRTHDATE -->
-            <div class="birthdate">
-                <label for="birthdate">Data di Nascita</label>
-                <input type="date" name="birthdate">
-            </div>
-            <!-- EMAIL -->
-            <div class="email">
-                <label for="email">Email</label>
-                <input type="text" name="email">
-            </div>
-            <!-- PASSWORD  -->
-            <div class="password">
-                <label for='password'>Password</label>
-                <input type='password' name='password'>
-            </div>
-            <!-- CONFIRM PASSWORD  -->
-            <div class="confirm_password">
-                <label for='confirm_password'>Ripeti la Password</label>
-                <input type='password' name='confirm_password'>
-            </div>
-            <!-- REMEMBER CHECKBOX  -->
-            <div class="remember">
-                <label for='remember'></label>Ricorda l'accesso</label>
-                <input type='checkbox' name='remember' value="1">
-            </div>
-            <div class="submit">
-                <input type='submit' value="Registrati">
-            </div>
-        </form>
-        <div class="error_display hidden"></div>
-        <div class="signup">Hai già un account ? <a href=" {{ route('login') }} ">Fai il login !</a></div>
+
+       <!-- FORM -->
+       <form name='signup' method='post'>
+           <!-- NAME -->
+           <div class="name">
+               <label for="name">Nome</label>
+               <input type="text" name="name">
+           </div>
+           <!-- SURNAME -->
+           <div class="surname">
+               <label for="surname">Cognome</label>
+               <input type="text" name="surname">
+           </div>
+           <!-- BIRTHDATE -->
+           <div class="birthdate">
+               <label for="birthdate">Data di Nascita</label>
+               <input type="date" name="birthdate">
+           </div>
+           <!-- EMAIL -->
+           <div class="email">
+               <label for="email">Email</label>
+               <input type="text" name="email">
+           </div>
+           <!-- PASSWORD  -->
+           <div class="password">
+               <label for='password'>Password</label>
+               <input type='password' name='password'>
+           </div>
+           <!-- CONFIRM PASSWORD  -->
+           <div class="confirm_password">
+               <label for='confirm_password'>Ripeti la Password</label>
+               <input type='password' name='confirm_password'>
+           </div>
+           <div class="submit">
+               <input type='submit' value="Registrati">
+           </div>
+       </form>
+       <div class="error_display hidden"></div>
+       <div class="signup">Hai già un account ? <a href=" {{ route('login') }} ">Fai il login !</a></div>
+
     </section>
 @endsection
