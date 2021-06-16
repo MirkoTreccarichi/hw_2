beba1099131e9ef02ceb8d4657bbfdaf5e833755
MIRKO TRECCARICHI <uni400949@studium.unict.it>
2021-06-16 16:38:19 +0200

    chech login and logout implemented

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,21 +4,44 @@
 namespace App\Http\Controllers;
 
 
+use App\models\Cliente;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
 class LoginController extends Controller
 {
     function login(){
-        $username = session('username');
         if(session('username'))
-            redirect('area_riservata');
+            return redirect('area_riservata');
         else
             return view('login');
     }
 
-    function checkLogin(){
-//        todo funzione checklogin controller
+    function checkLogin(Request $request){
+        if ($request->has('email') && $request->has('password')){
+            $utente = Cliente::where('email',$request->input('email'))->first();
+
+            if (password_verify($request->input('password'),$utente->password)){
+                session(['username','user_id'],[$request->input('email'),$utente->id]);
+                return redirect(route('customer_area'));
+            }
+
+            $error = "Username e/o password errati.";
+        }
+
+        if (!$request->hasAny('email','password')){
+            $error = "Inserisci username e password.";
+
+        }
+
+        //myemail/username : admin
+        //mypassword : admin
+        return $error;
+
     }
     function logout(){
-//        todo funzione logout controller
+        Session::flush();
+        return redirect(route('home'));
     }
 
 }
