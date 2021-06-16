b6c922cffafc1336fd4a0632ab2b1d92a61683e1
MIRKO TRECCARICHI <uni400949@studium.unict.it>
2021-06-16 18:40:55 +0200

    reserved area function created

--- app/Http/Controllers/ReservedAreaController.php
+++ app/Http/Controllers/ReservedAreaController.php
@@ -6,5 +6,11 @@ namespace App\Http\Controllers;
 
 class ReservedAreaController
 {
+    function reservedArea(){
+        if (LoginController::isLogged())
+            return view('reserved_area');
+        return redirect(route('login'));
+    }
+
 
 }
