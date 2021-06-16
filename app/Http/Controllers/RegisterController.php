1893af3c8202683d9aab4dd4b2708e60ade19fbf
MIRKO TRECCARICHI <uni400949@studium.unict.it>
2021-06-16 16:38:40 +0200

    registration implemented

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,12 +6,37 @@ namespace App\Http\Controllers;
 
 use App\models\Cliente;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Psy\Util\Json;
 
 class RegisterController extends Controller
 {
-    function register(){
-    //todo funzione di registrazione nel db
+    function register(Request $request){
+        if(session('_user_id'))
+            redirect(route('login'));
+        if(!$request->hasAny(['email','password','confirm_password','name','surname','birthdate']) ){
+            if (isset($_POST["email"]) || isset($_POST["password"]))
+                if(!$request->hasAny(['email','password']))
+                    $error = "Inserisci email e password.";
+        }
+
+        Http::post(route('emailFree'),['email',$request->input('email')]) ? : $error[] = "email già utilizzata";
+
+        if(isset($error))
+            return($error);
+
+        Cliente::create([
+            'nome'=>$request->input('name'),
+            'cognome'=>$request->input('surname'),
+            'data_nascita'=>$request->input('birthdate'),
+            'email'=>$request->input('email'),
+            'password'=>password_hash($request->input('password'),PASSWORD_BCRYPT)
+        ]);
+
+        return redirect(route('customer_area'));
+
+        //myemail/username : admin
+        //mypassword : admin
     }
 
     function page(){
@@ -24,7 +49,7 @@ class RegisterController extends Controller
 
         $cliente = Cliente::where('email',$request->input('email'))->first();
 
-        if($cliente != null) {
+        if($cliente !== null) {
             return Json::encode(false);
         }
         return Json::encode(true);
