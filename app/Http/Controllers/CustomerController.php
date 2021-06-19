b2c64127f6024255b0fd7b2e8165008a09892ef2
MIRKO TRECCARICHI <uni400949@studium.unict.it>
2021-06-19 22:45:03 +0200

    favoriteCustomerPoint function finished

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -4,7 +4,34 @@
 namespace App\Http\Controllers;
 
 
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Psy\Util\Json;
+
 class CustomerController extends Controller
 {
+    function favoriteCustomerPoint(Request $request){
+        $response = new JsonResponse();
+        if (empty($request->query())){
+            if (!$request->hasCookie('prefcities')){
+                return $response->withCookie('prefcities',null);
+            }
 
+            return $request->cookie('prefcities');
+        }
+
+        if (!$request->has('citta0')){
+            if ($request->hasCookie('prefcities')){
+                $response->cookie(cookie()->forever('prefcities',null));
+                return $response;
+            }
+        }
+
+        $response->withCookie(cookie()->forever('prefcities',
+            Json::encode($request->query())));
+        $response->setData($request->query());
+
+        return $response;
+    }
 }
