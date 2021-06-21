59f4c3fce6c7831a52eea4fba9e523725564acdc
MIRKO TRECCARICHI <uni400949@studium.unict.it>
2021-06-21 19:57:23 +0200

    customer list page finished

--- resources/views/customer_list.blade.php
+++ resources/views/customer_list.blade.php
@@ -1,14 +1,66 @@
 @extends("layouts.home")
 @section('title', ' | LISTA')
 @push('home_style')
-    <link rel="stylesheet" href="{{asset("styles/reserved_area.css")}}"> {{--todo cambiare css--}}
+    <link rel="stylesheet" href="{{asset("styles/customer_list.css")}}">
+    <link rel="stylesheet" href="{{asset("styles/reserved_area.css")}}">
 @endpush
 @push('scripts')
-    <script src="{{asset('js/script_list.js')}}" defer></script> {{--todo cambiare script--}}
+    <script src="{{asset('js/script_list.js')}}" defer></script>
 @endpush
 @section('buttons')
     <div class="link">
         <a href="{{route('home')}}">home</a>
         <a class="login" href="{{route('logout')}}">logout</a>
     </div>
+@endsection
+@section('content')
+
+
+    <section class="poster">
+
+
+        <h1>Pensaci oggi ritira domani !<a href="{{route('customer_area')}}">Oppure torna indietro</a></h1>
+
+        <h3>La tua lista</h3>
+        <ul class="lista">
+        @foreach($products as $prod)
+                <li>
+                    {{$prod['nome'].' '.$prod['produttore'].' X '.$prod['quantita_prodotto']}}
+                </li>
+            @endforeach
+        </ul>
+        <hr>
+
+        <div class="lists hidden">
+            <h3>Le tue liste</h3>
+
+            <div class="grid">
+
+            </div>
+        </div>
+
+
+        <div class="choosen hidden">
+            <h3>Le tue scelte</h3>
+            <input type="button" value="Salva">
+            <div class="grid">
+
+            </div>
+        </div>
+
+        <div class="choices">
+            <h3>Clicca sul carrello</h3>
+            <label>
+                Cerca i tuoi prodotti
+                <input type="search">
+            </label>
+
+            <div class="grid">
+
+            </div>
+        </div>
+
+    </section>
+
+
 @endsection
